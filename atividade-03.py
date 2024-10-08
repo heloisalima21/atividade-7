e075e912e07a4521cb65a7ab08c655d4daccf018
heloisalima21 <heloisalimassa@gmail.com>
2024-10-08 19:01:43 +0000

    atvdd

--- atividade-03.py
+++ atividade-03.py
@@ -10,6 +10,7 @@ nomes = []
 idades = []
 alturas = []
 pesos = []
+imc = []
 
 # Solicitando os dados dos usuários em um loop
 while True:
@@ -30,6 +31,31 @@ while True:
     alturas.append(altura)
     pesos.append(peso)
 
+# Função para calcular e mostrar classificação
+def calcular_imc (pesos, alturas):
+    return peso / (altura ** 2)
+
+def classificar_imc (imc):
+    if imc < 18.5:
+        "abaixo do peso"
+    elif 18.5 <= 25:
+        "peso normal"
+    elif 25 <= imc < 30:
+        "sobrepeso"    
+    elif 30 <= imc < 35:
+        "obesidade grau I"
+    elif 35 <= imc < 40:
+        "obesidade grau II"
+    elif imc >= 40:
+         "obesidade grau III"
+    return classificar_imc
+    
+# Exemplos de uso:
+peso = float(input("Digite seu peso em quilogramas: "))
+altura = float(input("Digite sua altura em metros: "))
+
+imc = calcular_imc(peso, altura)
+
 # Exibindo os dados armazenados
 logoSenai()
 print("\nDados dos usuários:")
@@ -39,4 +65,5 @@ for i in range(len(nomes)):
     print("Idade:", idades[i])
     print("Altura:", alturas[i], "metros")
     print("Peso:", pesos[i], "quilogramas")
-    print()
+    print(f"seu imc é : {imc: .2f}")
+    print(f"classificação: {classificar_imc}")
